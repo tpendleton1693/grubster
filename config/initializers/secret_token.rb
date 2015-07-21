8b3be2e936b05b763da7dbb6dd2a8d4b23cea107
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-07-21 14:22:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grubster::Application.config.secret_key_base = '7eb7dd3792a174a2b39cb1a1840e62df47bf1267560bc2d67ceb421e5a518869d757b1edd970da51cb9b4c1e743b4c3a538740af624272787209d5e976bf60c6'
