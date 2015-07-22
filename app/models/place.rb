9947b2a214e86bdc220efdeb5a1ce3f988af00d6
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-07-22 16:45:46 +0000

    Added google maps

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,12 @@
 class Place < ActiveRecord::Base
+  # Add associations
   belongs_to :user
+
+  # Geocoding
   geocoded_by :address
   after_validation :geocode
+
+  # Add Validation
   validates :name, :presence => true, length: { minimum: 3 }
   validates :address, :presence => true
   validates :description, :presence => true
