b97dea3ba273004e6d7a617b20829cda6d6b2a60
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-08-31 02:18:13 +0000

    Added test for humanized rating

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,8 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "humanized rating" do
+    comment = Comment.new(:rating => '1_star')
+    assert_equal 'One Star', comment.humanized_rating
+  end
 end
