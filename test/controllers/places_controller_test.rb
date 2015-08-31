ae8525eb8831ab5e9260517522783f38df2eaa55
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-08-31 00:50:14 +0000

    Added test for user authentication when creating a place

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -4,17 +4,19 @@ class PlacesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "create not signed in" do
+  test "user not signed in - create" do
     assert_no_difference 'Place.count' do
-    post :create, {:place => {
+      post :create, {:place => {
           :name => 'yolo',
           :description => 'omg',
-          :latitude => 42.3535353,
+          :latitude => 42.3631519,
           :longitude => -71.056098
-          }
         }
-      end
+      }
+    end
     assert_redirected_to new_user_session_path
+
+    assert_equal 0, Place.count
   end
 
 end
--- test/factories.rb
+++ test/factories.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
+  factory :user do
+    sequence :email do |n|
+      "sample#{n}@gmail.com"
+    end
+    password 'password'
+    password_confirmation 'password'
+  end
+
   factory :place do
-    name "Bowery Meat Co"
-    description "Best steak I've ever had"
-    address "9 E 1st St, New York, NY 10003"
+    name "Food"
+    description "Great stuff"
+    address "44 newhaven st, Beverly Hills CA, 90210"
   end
 end
