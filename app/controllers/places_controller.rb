7ac520b34c885ebc479ce857ad0ddc4165a770f8
vagrant <vagrant@precise32.(none)>
2015-09-10 00:53:56 +0000

    Fixed issues with editing and destroying places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,65 +1,66 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
-  def index
-    @places = Place.order(:id).page(params[:page]).per(5)
-  end
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
-  def new
-    @place = Place.new
-  end
+	def index
+		@places = Place.order(:id).page(params[:page]).per(5)
+	end
 
-  def create
-    @place = current_user.places.create(place_params)
-    if @place.valid?
-      redirect_to places_path
-    else
-      render :new, :status => :unprocessable_entity
-    end
-  end
+	def new
+		@place = Place.new
+	end
 
-  def show
-    @place = Place.find(params[:id])
-    @comment = Comment.new
-    @photo = Photo.new
-  end
+	def create
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 
-  def edit
-    @place = Place.find(params[:id])
-    if @place.user == current_user || current_user.admin?
-      return render :text => 'Not Allowed', :status => :forbidden
-    end
-  end
+	end
 
-  def update
-    @place = Place.find(params[:id])
-    if @place.user == current_user || current_user.admin?
-      @place.update_attributes(place_params)
-      if @place.valid?
-        redirect_to root_path
-      else
-        render :edit, :status => :unprocessable_entity
-      end
-    else
-      render :text => 'Not Allowed', :status => :forbidden
-    end
+	def show
+		@place = Place.find(params[:id])
+		@comment = Comment.new
+		@photo = Photo.new
+	end
+
+	def edit
+		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
+	end
+
+	def update
+		@place = Place.find(params[:id])
+			if @place.user != current_user
+				return render :text => 'Not Allowed', :status => :forbidden
+			end
+			@place.update_attributes(place_params)
+			if @place.valid?
+				redirect_to root_path
+			else
+				render :edit, :status => :unprocessable_entity
+			end
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+			if @place.user != current_user
+				return render :text => 'Not Allowed', :status => :forbidden
+			end
+		@place.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
 
 
-  end
-
-  def destroy
-    @place = Place.find(params[:id])
-    if @place.user == current_user || current_user.admin?
-      return render :text => 'Not Allowed', :status => :forbidden
-    end
-
-    @place.destroy
-    redirect_to root_path
-  end
-
-  private
-
-  def place_params
-    params.require(:place).permit(:name, :description, :address)
-  end
 end
